7c8c36bff449ab45b24fa33aafc2e4e1615fe011
Drayusni Aldiano <aldi.tkjsmeksa25@gmail.com>
2026-09-06 20:26:32 +0700

    Menambah Fitur Perulangan

---
+++ cek_ganjil_genap_perulangan.py
@@ -0,0 +1,7 @@
+for i in range(100):
+    angka = int(input("Masukkan bilangan: "))
+
+    if angka % 2 == 0:
+        print(f"{angka} adalah bilangan GENAP")
+    else:
+        print(f"{angka} adalah bilangan GANJIL")
